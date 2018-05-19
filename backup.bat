d99f9bdce86cbf1ed403a63800f85f78080ebcd5
cah4e3 <cah4e3@gmail.com>
2018-05-20 01:18:08 +0300

    current

--- backup.bat
+++ backup.bat
@@ -2,6 +2,7 @@ mkdir .\backup\current
 mkdir .\backup\current\nas
 mkdir .\backup\current\chr
 mkdir .\backup\current\doc
+mkdir .\backup\current\txt
 rem mkdir .\backup\current\tlm
 
 copy /b *.bat .\backup\current\*.bat
@@ -10,5 +11,6 @@ copy /b nas\*.exe .\backup\current\nas\*.exe
 copy /b nas\*.nas .\backup\current\nas\*.nas
 copy /b chr\*.chr .\backup\current\chr\*.chr
 copy /b doc\*.txt .\backup\current\doc\*.txt
+copy /b txt\*.* .\backup\current\txt\*.*
 rem copy /b tlm\*.* .\backup\current\tlm\*.*
 copy /b *.txt .\backup\current\*.txt
